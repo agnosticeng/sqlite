831d01cc776fe2f067581650cae441202dbee34d
didierfranc <didier@agnostic.engineering>
2025-01-02 11:49:01 +0100

    chore: setup ci

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       entry: resolve(__dirname, "src/sqlite.ts"),
       name: "SQLite",
       fileName: "sqlite",
+      formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
       external: ["@sqlite.org/sqlite-wasm"],
